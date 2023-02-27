8d7caa4be1a27196f2b44af1d9cdda667cd97cde
Justyna <justyna.sienkiewicz93@gmail.com>
2023-02-27 22:24:22 +0100

    ex 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import _ from 'lodash';
 
+const LS_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+let savedContent;
+
+const onLoadHandler = () => {
+  try {
+    const savedState = localStorage.getItem(LS_KEY);
+    if (savedState) {
+      const loadedContent = JSON.parse(savedState);
+      form.email.value = loadedContent.email;
+      form.message.value = loadedContent.message;
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const onInputHandler = () => {
+  savedContent = JSON.stringify({
+    email: form.email.value,
+    message: form.message.value,
+  });
+  localStorage.setItem(LS_KEY, savedContent);
+};
+
+const onSubmitHandler = e => {
+  e.preventDefault();
+  console.log(savedContent);
+  localStorage.removeItem(LS_KEY);
+  form.reset();
+};
+
+onLoadHandler();
+form.addEventListener('input', _.throttle(onInputHandler, 500));
+form.addEventListener('submit', onSubmitHandler);
