b1c748079ff073c11607a019084b192762fa9bb1
Justyna <justyna.sienkiewicz93@gmail.com>
2023-02-27 21:19:41 +0100

    ex 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const LS_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let time = localStorage.getItem(LS_TIME) || 0;
+
+player.setCurrentTime(time);
+
+const saveTime = data => {
+  localStorage.setItem(LS_TIME, data.seconds);
+};
+
+player.on(
+  'timeupdate',
+  _.throttle(data => saveTime(data), 1000)
+);
